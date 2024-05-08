37667061d2dfea408155ff504b869141603f6c07
caldo <gabcaldo@droidunb.com>
2024-05-08 15:47:58 -0300

    Códigos da visão e do movimento adicionados

---
+++ Código Visão/version.py
@@ -0,0 +1,19 @@
+#Código para verificação das versões do Pytorch e se a GPU está configurada
+#Precisamos de GPU para rodar o YOLOv8, então é importante verificar se está tudo certo
+
+
+
+
+import torch
+
+print(torch.__version__)
+
+print("GPU Configurada:", torch.cuda.is_available())
+print("Total de GPUs", torch.cuda.device_count())
+
+if torch.cuda.is_available():
+    print("GPU Atual:", torch.cuda.current_device())
+    print("Device", torch.cuda.device(0))
+    print("Device Name", torch.cuda.get_device_name(0))
+else:
+    print("Nenhuma GPU configurada")
