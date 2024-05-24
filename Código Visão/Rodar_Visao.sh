fb47de8985c7e7c33cc9a816ec16fc2397c1136f
caldo <gabcaldo@droidunb.com>
2024-05-23 21:53:34 -0300

    V 1.0

---
+++ Código Visão/Rodar_Visao.sh
@@ -0,0 +1,3 @@
+source "/home/caldo/Documents/Droid/VirtEnvTrekking/bin/activate" 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/caldo/Documents/Droid/VirtEnvTrekking/lib/python3.10/site-packages/nvidia/nvtx/lib
+python3 "ModelosDeTeste/Distancia_Posicionamento.py"
