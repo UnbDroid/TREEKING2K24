2a9949ccd79e0f1e77a8cb5008115d6c9d4d67c4
Caldo0-0 <gabcaldo@gmail.com>
2025-01-29 16:10:26 -0300

    Enxugado

--- build/CMakeFiles/clean_additional.cmake
+++ build/CMakeFiles/clean_additional.cmake
@@ -1,10 +1,8 @@
 # Additional clean files
 cmake_minimum_required(VERSION 3.16)
 
-if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "")
   file(REMOVE_RECURSE
-  "TREKKING-2024-2025.bin"
-  "TREKKING-2024-2025.map"
   "bootloader/bootloader.bin"
   "bootloader/bootloader.elf"
   "bootloader/bootloader.map"
@@ -18,6 +16,8 @@ if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
   "flasher_args.json"
   "ldgen_libraries"
   "ldgen_libraries.in"
+  "project-name.bin"
+  "project-name.map"
   "project_elf_src_esp32.c"
   "x509_crt_bundle.S"
   )
